453faeb2b9a8d3f92da4e2c0c111228dfd60ea14
ikkikana <hei.princy@gmail.com>
2026-09-18 14:21:49 +0300

    1 et 2.2 à jour.

--- Exercice1.js
+++ Exercice1.js
@@ -1,12 +1,28 @@
-function NegatifPositif(Nombre) {
-    let gauche= 0;
-    let droite= Nombre.length - 1;
+T = [3, -1, 0, 4, -2, 5, -3];
 
+function NegatifPositif(T) {
+    let gauche = 0;
+    let droite = Tableau.length - 1;
     while (gauche <= droite) {
+        if (Tableau[gauche] <= 0) {
+            gauche++;
+        }
+        else if (Tableau[droite] > 0) {
+            droite--;
+        }
+        else {
+            let  tri= T[gauche];
 
+
+            T[droite] = tri;
+            T[gauche] = T[droite];
+            gauche++;
+            droite--;
+        }
     }
+    return T;
 }
-Nombre = [3, -1, 0, 4, -2, 5, -3];
+
 
 
 
--- Exercice2.2.js
+++ Exercice2.2.js
@@ -1,3 +1,17 @@
 function Ranger(T) {
+    let cotéGauche = 0;
+    let cotéDroite = T.length - 1;
 
+    while (cotéGauche < cotéDroite) {
+        if (T[cotéGauche] === 0) {
+            cotéGauche++;
+        } else if (T[cotéDroite] === 1) {
+            cotéDroite--;
+        } else {
+            Echanger(T, cotéGauche, cotéDroite);
+            cotéGauche++;
+            cotéDroite--;
+        }
+    }
+    return T;
 }
